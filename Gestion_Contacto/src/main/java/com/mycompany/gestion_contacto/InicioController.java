b021bac11c7c2055a39ac804fa7f1620d72ca023
emjorome <emjorome@espol.edu.ec>
2023-10-31 18:02:54 -0500

    Pantalla inicio

---
+++ Gestion_Contacto/src/main/java/com/mycompany/gestion_contacto/InicioController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.gestion_contacto;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO
+ */
+public class InicioController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    ArrayList<String> lstUsuarios;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lstUsuarios = new ArrayList<>();
+    }
+
+    public void cargarUsuarios(){
+        try(ObjectInputStream oj = new ObjectInputStream(new FileInputStream("archivo.text"))){
+            lstUsuarios = (ArrayList<String>) oj.readObject();
+        }catch(FileNotFoundException f){
+            f.printStackTrace();
+        }catch(IOException io){
+            io.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } 
+    }
+    
+    public void crearNuevoUsuario(){
+        
+    }
+    
+}
