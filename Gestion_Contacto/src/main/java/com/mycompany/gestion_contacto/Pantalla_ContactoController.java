e19a38c2c36921285f541ac87475162baaf9fb95
unknown <djveliz@espol.edu.ec>
2023-11-27 21:08:27 -0500

    Alternatica 1

--- Gestion_Contacto/src/main/java/com/mycompany/gestion_contacto/Pantalla_ContactoController.java
+++ Gestion_Contacto/src/main/java/com/mycompany/gestion_contacto/Pantalla_ContactoController.java
@@ -6,6 +6,7 @@ package com.mycompany.gestion_contacto;
 
 import TDA.List;
 import TDA.ArrayList;
+import TDA.DoublyLinkedList;
 import java.util.LinkedList;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -80,6 +81,12 @@ public class Pantalla_ContactoController implements Initializable {
     @FXML
     private Label correo;
     
+    static DoublyLinkedList<Contacto> LCDE= new DoublyLinkedList<>();
+      Contacto SigContacto ;
+    Contacto SigContacto1 ;
+    Contacto SigContacto2 ;
+    Contacto SigContacto3 ;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        // cargarContactoPrincipal();
@@ -105,13 +112,12 @@ public class Pantalla_ContactoController implements Initializable {
                contador++;
                 if (contador==1) {
                     break;
-                    
                 }
             }
         } catch (Exception e) {
         }
-        
-
+        iniciaLCDE();
+        SigContacto= LCDE.get(0) ;
     }
 
     public void cargarContactoPrincipal(){
@@ -209,45 +215,113 @@ public class Pantalla_ContactoController implements Initializable {
     VBox vboxcontact = fxmlloader.load();
     Plantilla_contactoController controlador = fxmlloader.getController();
     controlador.setData(AntContacto);
-
+    
     // Limpia los nodos hijos antes de agregar el nuevo VBox
     hbox_contactos.getChildren().clear();
     hbox_contactos.getChildren().add(vboxcontact);
-
-        
     }
+//
+//    @FXML
+//    private void contacto_siguiente(ActionEvent event) throws IOException {
+//  
+//         if (lstContacto.isEmpty()) {
+//        // No hay elementos en la lista, manejar según sea necesario
+//        return;
+//    }
+//         
+//    currentIndex = (currentIndex + 1) % lstContacto.size();
+//    Contacto SigContacto = lstContacto.get(currentIndex);
+//    Contacto SigContacto1 = lstContacto.get(1);
+//    
+//    nombreMain.setText(SigContacto.getNombre()+" "+SigContacto.getApellido());
+//    nombreC.setText(SigContacto.getListTelefonos().get(0).getNumeroTelefono());
+//    trabocell.setText(SigContacto.getListTelefonos().get(1).getNumeroTelefono());
+//    
+//
+//    FXMLLoader fxmlloader = new FXMLLoader();
+//    fxmlloader.setLocation(getClass().getResource("plantilla_contacto.fxml"));
+//    VBox vboxcontact = fxmlloader.load();
+//    Plantilla_contactoController controlador = fxmlloader.getController();
+//    controlador.setData(SigContacto);
+//   
+//    
+//    VBox vboxcontact1 = fxmlloader.load();
+//    Plantilla_contactoController controlador1 = fxmlloader.getController();
+//    controlador1.setData(SigContacto1);
+//    
+//    // Limpia los nodos hijos antes de agregar el nuevo VBox
+//    hbox_contactos.getChildren().clear();
+//    hbox_contactos.getChildren().add(vboxcontact);
+//    hbox_contactos.getChildren().add(vboxcontact1);
+//
+//    }
+@FXML
+private void contacto_siguiente(ActionEvent event) throws IOException {
 
-
-    
-    @FXML
-    private void contacto_siguiente(ActionEvent event) throws IOException {
-  
-         if (lstContacto.isEmpty()) {
+    if (lstContacto.isEmpty()) {
         // No hay elementos en la lista, manejar según sea necesario
         return;
     }
-         
-    currentIndex = (currentIndex + 1) % lstContacto.size();
-    Contacto SigContacto = lstContacto.get(currentIndex);
-    
-    nombreMain.setText(SigContacto.getNombre()+" "+SigContacto.getApellido());
-    nombreC.setText(SigContacto.getListTelefonos().get(0).getNumeroTelefono());
-    trabocell.setText(SigContacto.getListTelefonos().get(1).getNumeroTelefono());
     
 
+    //currentIndex = (currentIndex + 1) % lstContacto.size();
+   
+    SigContacto1 = SigContacto;
+    SigContacto2 = LCDE.obtenerSiguiente(SigContacto);
+    SigContacto3 = LCDE.obtenerSiguiente(LCDE.obtenerSiguiente(SigContacto));
+    SigContacto= LCDE.obtenerPrevio(SigContacto);
+
+
+    nombreMain.setText(SigContacto.getNombre() + " " + SigContacto.getApellido());
+    nombreC.setText(SigContacto.getListTelefonos().get(0).getNumeroTelefono());
+    trabocell.setText(SigContacto.getListTelefonos().get(1).getNumeroTelefono());
+
+    // Cargar el archivo FXML una vez
     FXMLLoader fxmlloader = new FXMLLoader();
     fxmlloader.setLocation(getClass().getResource("plantilla_contacto.fxml"));
+
+    // Cargar la primera instancia
     VBox vboxcontact = fxmlloader.load();
     Plantilla_contactoController controlador = fxmlloader.getController();
     controlador.setData(SigContacto);
 
-    // Limpia los nodos hijos antes de agregar el nuevo VBox
+    // Crear un nuevo FXMLLoader para la segunda instancia
+    FXMLLoader fxmlloader1 = new FXMLLoader();
+    fxmlloader1.setLocation(getClass().getResource("plantilla_contacto.fxml"));
+
+    // Cargar la segunda instancia
+    VBox vboxcontact1 = fxmlloader1.load();
+    Plantilla_contactoController controlador1 = fxmlloader1.getController();
+    controlador1.setData(SigContacto1);
+
+    // Crear un nuevo FXMLLoader para la tercera instancia
+    FXMLLoader fxmlloader2 = new FXMLLoader();
+    fxmlloader2.setLocation(getClass().getResource("plantilla_contacto.fxml"));
+
+    // Cargar la tercera instancia
+    VBox vboxcontact2 = fxmlloader2.load();
+    Plantilla_contactoController controlador2 = fxmlloader2.getController();
+    controlador2.setData(SigContacto2);
+
+    // Crear un nuevo FXMLLoader para la cuarta instancia
+    FXMLLoader fxmlloader3 = new FXMLLoader();
+    fxmlloader3.setLocation(getClass().getResource("plantilla_contacto.fxml"));
+
+    // Cargar la cuarta instancia
+    VBox vboxcontact3 = fxmlloader3.load();
+    Plantilla_contactoController controlador3 = fxmlloader3.getController();
+    controlador3.setData(SigContacto3);
+
+    // Limpia los nodos hijos antes de agregar los nuevos VBox
     hbox_contactos.getChildren().clear();
-    hbox_contactos.getChildren().add(vboxcontact);
+    hbox_contactos.getChildren().addAll(vboxcontact, vboxcontact1, vboxcontact2, vboxcontact3);
+}
 
-    
-    
-
+public  void iniciaLCDE(){
+    for (int i = 0; i < lstContacto.size(); i++) {
+        LCDE.add(lstContacto.get(i));
     }
+
+}
     
 }
